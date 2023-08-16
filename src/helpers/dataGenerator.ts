004085740e50fbd37be0a605f1e462aea7b67ec4
danielcotes2504 <danielenrique2000@hotmail.com>
2023-08-16 11:19:53 -0500

    Added fake categories

--- src/helpers/dataGenerator.ts
+++ src/helpers/dataGenerator.ts
@@ -18,32 +18,9 @@ const generateFakeCategories = async (): Promise<void> => {
 
   await Categoria.bulkCreate(categoryData);
 };
-
-const generateFakePosts = async (): Promise<void> => {
-  const postData = [];
-  for (let i = 1; i <= 10; i++) {
-    const post = { id: i, titulo: randFileName(), contenido: randText() };
-    postData.push(post);
-  }
-
-  await Post.bulkCreate(postData);
-};
-
-const generateFakeComments = async (): Promise<void> => {
-  const commentData = [];
-  for (let i = 1; i <= 10; i++) {
-    const comment = { id: i, posts_id: i, contenido: randText() };
-    commentData.push(comment);
-  }
-
-  await Comentario.bulkCreate(commentData);
-};
-
 const generateFakeData = async (): Promise<void> => {
   try {
     await generateFakeCategories();
-    await generateFakePosts();
-    await generateFakeComments();
   } catch (error) {
     console.log(error)
     console.error("Error at generation of fake data");
